d29f23117780b84c36b98285cdaa5a324a816485
enoenot999@yandex.ru <enoenot999@yandex.ru>
2021-12-02 13:50:38 +0300

    Добавьте файлы проекта.

---
+++ calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    public static class Calc
+    {
+        public static double Plus (double x, double y)
+        {
+            return x + y;
+        }
+        public static double Minus(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Mult(double x, double y)
+        {
+            return x * y;
+        }
+        public static double Div(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
